e6b0996e127df0449c36ac3a66b8072b42f48d5e
Max Shen <maxwshen@gmail.com>
2018-04-27 11:02:40 -0400

    minimally sufficient features: genotype column, basic error catching, indel length fq, frameshift fq, precision, stats, private methods, highest frequency events

--- inDelphi.py
+++ inDelphi.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 import pickle, copy
 from scipy.stats import entropy
 
-model = None
+init_flag = False
 nn_params = None
 nn2_params = None
 normalizer = None
@@ -14,30 +14,30 @@ bp_model = None
 
 
 ##
-# NN
+# Private NN methods
 ##
-def sigmoid(x):
+def __sigmoid(x):
   return 0.5 * (np.tanh(x) + 1.0)
 
-def nn_function(params, inputs):
+def __nn_function(params, inputs):
   # """Params is a list of (weights, bias) tuples.
   #    inputs is an (N x D) matrix."""
   inpW, inpb = params[0]
-  inputs = sigmoid(np.dot(inputs, inpW) + inpb)
+  inputs = __sigmoid(np.dot(inputs, inpW) + inpb)
   for W, b in params[1:-1]:
     outputs = np.dot(inputs, W) + b
-    inputs = sigmoid(outputs)
+    inputs = __sigmoid(outputs)
   outW, outb = params[-1]
   outputs = np.dot(inputs, outW) + outb
   return outputs.flatten()
 
 ##
-# Sequence featurization
+# Private sequence featurization
 ##
-def get_gc_frac(seq):
+def __get_gc_frac(seq):
   return (seq.count('C') + seq.count('G')) / len(seq)
 
-def find_microhomologies(left, right):
+def __find_microhomologies(left, right):
   start_idx = max(len(right) - len(left), 0)
   mhs = []
   mh = [start_idx]
@@ -50,14 +50,14 @@ def find_microhomologies(left, right):
   mhs.append(mh)
   return mhs
 
-def featurize(seq, cutsite, DELLEN_LIMIT = 60):
-  # print 'Using DELLEN_LIMIT = %s' % (DELLEN_LIMIT)
+def __featurize(seq, cutsite, DELLEN_LIMIT = 60):
+  # print('Using DELLEN_LIMIT = %s' % (DELLEN_LIMIT))
   mh_lens, gc_fracs, gt_poss, del_lens = [], [], [], []
   for del_len in range(1, DELLEN_LIMIT):
     left = seq[cutsite - del_len : cutsite]
     right = seq[cutsite : cutsite + del_len]
 
-    mhs = find_microhomologies(left, right)
+    mhs = __find_microhomologies(left, right)
     for mh in mhs:
       mh_len = len(mh) - 1
       if mh_len > 0:
@@ -67,7 +67,7 @@ def featurize(seq, cutsite, DELLEN_LIMIT = 60):
         s = cutsite - del_len + gtpos - mh_len
         e = s + mh_len
         mh_seq = seq[s : e]
-        gc_frac = get_gc_frac(mh_seq)
+        gc_frac = __get_gc_frac(mh_seq)
 
         mh_lens.append(mh_len)
         gc_fracs.append(gc_frac)
@@ -76,31 +76,50 @@ def featurize(seq, cutsite, DELLEN_LIMIT = 60):
   return mh_lens, gc_fracs, gt_poss, del_lens
 
 ##
-# Prediction
+# Error catching
 ##
-def predict(seq, cutsite):
-  # Predict 1 bp insertions and all deletions (MH and MH-less)
-  # Most complete "version" of inDelphi
-  # Requires rate_model (k-NN) to predict 1 bp insertion rate compared to deletion rate
-  # Also requires bp_model to predict 1 bp insertion genotype given -4 nucleotide
+def error_catching(seq, cutsite):
+  # Type errors
+  if type(seq) != str:
+    return True, 'Sequence is not a string.'
+  if type(cutsite) != int:
+    return True, 'Cutsite is not an int.'
+  
+  # Cutsite bounds errors
+  if cutsite < 1 or cutsite > len(seq) - 1:
+    return True, 'Cutsite index is not within the sequence. Cutsite must be an integer between index 1 and len(seq) - 1, inclusive.'
 
+  # Sequence string errors
+  for c in set(seq):
+    if c not in list('ACGT'):
+      return True, 'Only ACGT characters allowed: Bad character %s' % (c)
+
+
+  return False, ''
+
+def provide_warnings(seq, cutsite):
+  if len(seq) <= 10:
+    print('Warning: Sequence length is very short (%s bp)' % (len(seq)))
+  return
+
+##
+# Private prediction methods
+##
+def __predict_dels(seq, cutsite):
   ################################################################
   #####
   ##### Predict MH and MH-less deletions
   #####
   # Predict MH deletions
 
-  if nn_params == None:
-    init_model()
-
-  mh_len, gc_frac, gt_pos, del_len = featurize(seq, cutsite)
+  mh_len, gc_frac, gt_pos, del_len = __featurize(seq, cutsite)
 
   # Form inputs
   pred_input = np.array([mh_len, gc_frac]).T
   del_lens = np.array(del_len).T
   
   # Predict
-  mh_scores = nn_function(nn_params, pred_input)
+  mh_scores = __nn_function(nn_params, pred_input)
   mh_scores = mh_scores.reshape(mh_scores.shape[0], 1)
   Js = del_lens.reshape(del_lens.shape[0], 1)
   unfq = np.exp(mh_scores - 0.25*Js)
@@ -111,7 +130,7 @@ def predict(seq, cutsite):
   for jdx in range(len(mh_vector)):
     if del_lens[jdx] == mh_vector[jdx]:
       dl = del_lens[jdx]
-      mhless_score = nn_function(nn2_params, np.array(dl))
+      mhless_score = __nn_function(nn2_params, np.array(dl))
       mhless_score = np.exp(mhless_score - 0.25*dl)
       mask = np.concatenate([np.zeros(jdx,), np.ones(1,) * mhless_score, np.zeros(len(mh_vector) - jdx - 1,)])
       mhfull_contribution = mhfull_contribution + mask
@@ -127,7 +146,7 @@ def predict(seq, cutsite):
   ##### Predict MH and MH-less deletions
   #####
   # Predict MH-less deletions
-  mh_len, gc_frac, gt_pos, del_len = featurize(seq, cutsite)
+  mh_len, gc_frac, gt_pos, del_len = __featurize(seq, cutsite)
 
   unfq = list(unfq)
 
@@ -146,7 +165,7 @@ def predict(seq, cutsite):
 
   mh_vector = np.array(mh_len)
   for dl in nonfull_dls:
-    mhless_score = nn_function(nn2_params, np.array(dl))
+    mhless_score = __nn_function(nn2_params, np.array(dl))
     mhless_score = np.exp(mhless_score - 0.25*dl)
 
     unfq.append(mhless_score)
@@ -159,10 +178,12 @@ def predict(seq, cutsite):
   nfq = np.divide(unfq, np.sum(unfq))  
   pred_freq = list(nfq.flatten())
 
-  d = {'Length': pred_del_len, 'Genotype Position': pred_gt_pos, 'Predicted_Frequency': pred_freq}
+  d = {'Length': pred_del_len, 'Genotype position': pred_gt_pos, 'Predicted frequency': pred_freq}
   pred_del_df = pd.DataFrame(d)
   pred_del_df['Category'] = 'del'
+  return pred_del_df, total_phi_score
 
+def __predict_ins(seq, cutsite, pred_del_df, total_phi_score):
   ################################################################
   #####
   ##### Predict Insertions
@@ -171,7 +192,7 @@ def predict(seq, cutsite):
   dlpred = []
   for dl in range(1, 28+1):
     crit = (pred_del_df['Length'] == dl)
-    dlpred.append(sum(pred_del_df[crit]['Predicted_Frequency']))
+    dlpred.append(sum(pred_del_df[crit]['Predicted frequency']))
   dlpred = np.array(dlpred) / sum(dlpred)
   norm_entropy = entropy(dlpred) / np.log(len(dlpred))
   precision = 1 - norm_entropy
@@ -205,47 +226,158 @@ def predict(seq, cutsite):
     pred_1bpins_d['Category'].append('ins')
     pred_1bpins_d['Length'].append(1)
     pred_1bpins_d['Inserted Bases'].append(ins_base)
-    pred_1bpins_d['Predicted_Frequency'].append(freq)
+    pred_1bpins_d['Predicted frequency'].append(freq)
 
   pred_1bpins_df = pd.DataFrame(pred_1bpins_d)
-  pred_all_df = pred_del_df.append(pred_1bpins_df, ignore_index = True)
-  pred_all_df['Predicted_Frequency'] /= sum(pred_all_df['Predicted_Frequency'])
+  pred_df = pred_del_df.append(pred_1bpins_df, ignore_index = True)
+  pred_df['Predicted frequency'] /= sum(pred_df['Predicted frequency'])
+  return pred_df
 
-  return pred_del_df, pred_all_df, total_phi_score, rate_1bpins
+def __build_stats(seq, cutsite, pred_df, total_phi_score):
+  overall_precision = 1 - entropy(pred_df['Predicted frequency']) / np.log(len(pred_df))
+  highest_fq = max(pred_df['Predicted frequency'])
+  ins_fq = sum(pred_df[pred_df['Category'] == 'ins']['Predicted frequency'])
+  stats = {'Phi': total_phi_score,
+           'Phi percentile': None,
+           'Precision:': overall_precision,
+           'Precision percentile': None,
+           '1-bp ins frequency': ins_fq,
+           '1-bp ins frequency percentile': None,
+           'Highest outcome frequency': highest_fq,
+           'Highest outcome frequency percentile': None,
+           'Reference sequence': seq,
+           'Cutsite': cutsite,
+           'gRNA': None,
+           'gRNA orientation': None,
+           'Cas9 type': None,
+          }
+  return stats
+
+##
+# Main public-facing prediction
+##
+def predict(seq, cutsite):
+  # Predict 1 bp insertions and all deletions (MH and MH-less)
+  if init_flag == False:
+    init_model()
+
+  # Sanitize input
+  seq = seq.upper()
+  flag, error = error_catching(seq, cutsite)
+  if flag:
+    return error
+  provide_warnings(seq, cutsite)
+
+
+  # Make predictions
+  pred_del_df, total_phi_score = __predict_dels(seq, cutsite)
+  pred_df = __predict_ins(seq, cutsite, 
+                              pred_del_df, total_phi_score)
+
+  # Build stats
+  stats = __build_stats(seq, cutsite, pred_df, total_phi_score)
+  
+  return pred_df, stats
 
 ##
 # Process predictions
 ##
-def get_frameshift_fqs(pred_all_df):
+def get_frameshift_fqs(pred_df):
+  # Returns a dataframe
+  #   - Frame
+  #   - Predicted frequency
+  #
   fsd = {'+0': 0, '+1': 0, '+2': 0}
 
-  crit = (pred_all_df['Category'] == 'ins')
-  ins1_fq = sum(pred_all_df[crit]['Predicted_Frequency'])
+  crit = (pred_df['Category'] == 'ins')
+  ins1_fq = sum(pred_df[crit]['Predicted frequency'])
   fsd['+1'] += ins1_fq
 
-  for del_len in set(pred_all_df['Length']):
-    crit = (pred_all_df['Category'] == 'del') & (pred_all_df['Length'] == del_len)
-    fq = sum(pred_all_df[crit]['Predicted_Frequency'])
+  for del_len in set(pred_df['Length']):
+    crit = (pred_df['Category'] == 'del') & (pred_df['Length'] == del_len)
+    fq = sum(pred_df[crit]['Predicted frequency'])
     fs = (-1 * del_len) % 3
     fsd['+%s' % (fs)] += fq
 
-  return fsd
+  d = defaultdict(list)
+  d['Frame'] = fsd.keys()
+  d['Predicted frequency'] = fsd.values()
+  df = pd.DataFrame(d)
+  df = df.sort_values(by = 'Frame')
+  df.reset_index(drop = True)
+  return df
+
+def get_indel_length_fqs(pred_df):
+  # Returns a dataframe
+  #   - Indel length
+  #   - Predicted frequency
+  d = defaultdict(list)
+
+  crit = (pred_df['Category'] == 'ins')
+  ins1_fq = sum(pred_df[crit]['Predicted frequency'])
+  d['Indel Length'].append('+1')
+  d['Predicted frequency'].append(ins1_fq)
+
+  for del_len in set(pred_df['Length']):
+    crit = (pred_df['Category'] == 'del') & (pred_df['Length'] == del_len)
+    fq = sum(pred_df[crit]['Predicted frequency'])
+    d['Indel Length'].append('-%s' % (del_len))
+    d['Predicted frequency'].append(fq)
+
+  df = pd.DataFrame(d)
+  return df  
+
+def get_highest_frequency_indel(pred_df):
+  # Returns a row of pred_df
+  highest_fq = max(pred_df['Predicted frequency'])
+  row = pred_df[pred_df['Predicted frequency'] == highest_fq]
+  return row.iloc[0]
+
+def get_highest_frequency_length(pred_df):
+  idd = get_indel_length_fqs(pred_df)
+  highest_fq = max(idd['Predicted frequency'])
+  row = idd[idd['Predicted frequency'] == highest_fq]
+  return row.iloc[0]
+
+##
+# Data reformatting
+##
+def add_genotype_column(pred_df, stats):
+  gts = []
+  seq = stats['Reference sequence']
+  cutsite = stats['Cutsite']
+
+  for idx, row in pred_df.iterrows():
+    gt_pos = row['Genotype position']
+    if gt_pos == 'e':
+      gt = np.nan
+    elif row['Category'] == 'del':
+      dl = row['Length']
+      gt = seq[:cutsite - dl + gt_pos] + seq[cutsite + gt_pos:]
+    else:
+      ins_base = row['Inserted Bases']
+      gt = seq[:cutsite] + ins_base + seq[cutsite:]
+    gts.append(gt)
+  pred_df['Genotype'] = gts
+  return
+
 
 
 ##
 # Init
 ##
 def init_model(run_iter = 'abf', param_iter = 'aag'):
-  global nn_params
-  if nn_params != None:
+  global init_flag
+  if init_flag != False:
     return
 
-  print 'Initializing model %s/%s...' % (run_iter, param_iter)
+  print('Initializing model %s/%s...' % (run_iter, param_iter))
 
+  global nn_params
+  global nn2_params
   nn_params = pickle.load(open('%s_%s_nn.pkl' % (run_iter, 
                                               param_iter,
                                               )))
-  global nn2_params
   nn2_params = pickle.load(open('%s_%s_nn2.pkl' % (run_iter, 
                                               param_iter,
                                               )))
@@ -257,6 +389,8 @@ def init_model(run_iter = 'abf', param_iter = 'aag'):
   rate_model = pickle.load(open('rate_model_%s.pkl' % ('v3')))
   normalizer = pickle.load(open('normalizer_%s.pkl' % ('v3')))
 
-  print 'Done'
+  init_flag = True
+
+  print('Done')
   return
 
